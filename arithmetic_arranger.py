a665d25da716048aa234ccadbc2d8c29ac592664
tonycc <tonycamezcha@gmail.com>
2022-11-13 17:43:55 -0600

    feat: changed arithmetic_arranger function structure, finished situations that will return an error

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,35 +1,28 @@
+
 def arithmetic_arranger(lst_operations, output=False):
     arranged = ''
 
     if len(lst_operations) > 5:
         return print('Error: Too many problems.')
 
+    operators = list(map(lambda x: x.split()[1], lst_operations))
+
+    if set(operators) != {'-', '+'}:
+        return print('Error: Operator must be \'+\' or \'-\'.')
+
     for operation in lst_operations:
-        if '+' in operation:
-            digits = operation.split('+')
-            for digit in digits:
-                digit = digit.strip()
-                if len(digit) > 4:
-                    return print('Error: Numbers cannot be more than four digits.')
-                try:
-                    digit = int(digit)
-                    # print(digit)
-                except ValueError:
-                    return print('Error: Numbers must only contain digits.')
-            
-        elif '-' in operation:
-            digits = operation.split('-')
-            for digit in digits:
-                digit = digit.strip()
-                if len(digit) > 4:
-                    return print('Error: Numbers cannot be more than four digits.')
-                try:
-                    digit = int(digit)
-                    # print(digit)
-                except ValueError:
-                    return print('Error: Numbers must only contain digits.')
-        else:
-            return print('Error: Operator must be \'+\' or \'-\'.')
+        operator = operation.split()[1]
+        digits = operation.split(operator)
+        # print(digits)
+        for digit in digits:
+            digit = digit.strip()
+            if len(digit) > 4:
+                return print('Error: Numbers cannot be more than four digits.')
+            try:
+                digit = int(digit)
+                # print(digit)
+            except ValueError:
+                return print('Error: Numbers must only contain digits.')
             
 
 arithmetic_arranger(['3 + 5', '223 - 5', '224 - 6233'])
